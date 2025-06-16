bcd49da706342ad7af08c976e092f7543dcb9da5
mackmother <48323926+mackmother@users.noreply.github.com>
2025-06-15 21:47:19 -0500

    chore(web): enable Tailwind and dark theme styling

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: "#F6B352",
+          secondary: "#F68657",
+          dark1: "#383A3F",
+          dark2: "#1F2124",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
